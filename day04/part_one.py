6e98751a0799debe9cbaaca15269ade909644e69
Oscar <oscar@masse.pro>
2022-12-17 20:28:51 +0100

    solve day 4

---
+++ day04/part_one.py
@@ -0,0 +1,21 @@
+from utils import load_input
+
+
+def solve():
+    input_ = load_input()
+    pairs = [tuple(list_sections(sections) for sections in line.split(',')) for line in input_.splitlines()]
+    count = 0
+    for first, second in pairs:
+        if set(first).issubset(second) or set(second).issubset(first):
+            count += 1
+
+    print(count)
+
+
+def list_sections(sections):
+    split = sections.split('-')
+    return [*range(int(split[0]), int(split[1]) + 1)]
+
+
+if __name__ == '__main__':
+    solve()
