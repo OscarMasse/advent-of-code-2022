a60377b274ebf2c86955a28a98edcef02d8ce84c
OscarMasse <oscar@masse.pro>
2022-12-22 22:42:24 +0100

    improve generation

--- generator.py
+++ generator.py
@@ -11,19 +11,23 @@ def generate(session_cookie=load_session_cookie()):
         print(f"Generating Day {number:02}")
         dir_name = f"day{number:02}"
 
+        response = None
+        if not os.path.isfile(f"{dir_name}/input.txt"):
+            response = requests.get(f"https://adventofcode.com/2022/day/{number}/input",
+                                    headers={"cookie": f"session={session_cookie}"})
+            if not response.status_code == 200:
+                continue
+
         # Directory
         if not os.path.isdir(f"{dir_name}"):
             print(f"• Creating directory")
             os.makedirs(f"{dir_name}", exist_ok=True)
 
         # Input file
-        if not os.path.isfile(f"{dir_name}/input.txt"):
-            response = requests.get(f"https://adventofcode.com/2022/day/{number}/input",
-                                    headers={"cookie": f"session={session_cookie}"})
-            if response.status_code == 200:
-                print(f"• Downloading input.txt")
-                with open(f"{dir_name}/input.txt", "wb") as file:
-                    file.write(response.content)
+        if response:
+            print(f"• Creating input.txt")
+            with open(f"{dir_name}/input.txt", "wb") as file:
+                file.write(response.content)
 
         # Python files
         for file_name in ["part_one.py", "part_two.py"]:
